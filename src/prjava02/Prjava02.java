eac0211e7f4fcfc1a3c0ea5008ec9d8f8825518f
SergiLobo <sergi.llop.cmrm12@gmail.com>
2016-04-21 20:23:20 +0200

    Treballant amb ordres Git i branques v0.2

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -11,6 +11,8 @@ import java.net.*;
 	     */
 	    public static void main(String[] args) {
 	        System.out.println("versió 0.1 del projecte prjava02"); 
+	        System.out.println("Creació d'una branca del projecte prjava02");
+	        System.out.println("Afegint més codi a la branca00 del projecte prjava02");
 	        try {
 	            InetAddress adreça = InetAddress.getLocalHost();
 	            String hostname = adreça.getHostName();
